d448cf335e038475c722fb83609bd646dcd86190
Natalia Mazur-Zurek <nataliamazur1988@gmail.com>
2022-08-02 11:19:14 +0200

    update js function

--- js/script.js
+++ js/script.js
@@ -1,43 +1,63 @@
-console.log("Cześć!");
-
-let exchange = document.querySelector(".js-exchange");
-let exchangeButton = document.querySelector(".js-exchangeButton");
-
-exchangeButton.addEventListener("click", () => {
-    exchange.classList.toggle("js-exchange");
-    exchangeButton.innerText = exchange.classList.contains("js-exchange")
-        ? "Pokaż aktualne kursy:" : "Ukryj aktualne kursy:"
-});
-
-let amountElement = document.querySelector(".js-amount");
-let currencyElement = document.querySelector(".js-currency");
-let formElement = document.querySelector(".js-form");
-let resultElement = document.querySelector(".js-result");
-
-formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
-
-    let amount = +amountElement.value;
-    let currency = currencyElement.value;
-    let rateEUR = 4.7552;
-    let rateUSD = 4.6916;
-    let rateCHF = 4.8739;
-
-    switch (currency) {
-
-        case "EUR":
-            result = amount / rateEUR;
-            break;
-
-        case "USD":
-            result = amount / rateUSD;
-            break;
-
-        case "CHF":
-            result = amount / rateCHF;
-            break;
+{
+    const welcome = () => {
+        console.log("Cześć!");
     };
 
-    resultElement.innerHTML = `${amount.toFixed(2)} PLN = <strong>${result.toFixed(2)} ${currency}</strong>`
-});
+    const toggleCurrency = () => {
+        const exchange = document.querySelector(".js-exchange");
+        const themeNameButtonCurrency = document.querySelector(".js-themeNameButtonCurrency");
+        exchange.classList.toggle("exchange");
+        themeNameButtonCurrency.innerText = exchange.classList.contains("exchange")
+            ? "Pokaż" : "Ukryj"
+    };
 
+    const calculateResult = (amount, currency) => {
+        const rateEUR = 4.7552;
+        const rateUSD = 4.6916;
+        const rateCHF = 4.8739;
+
+        switch (currency) {
+
+            case "EUR":
+                return amount / rateEUR;
+
+
+            case "USD":
+                return amount / rateUSD;
+
+
+            case "CHF":
+                return amount / rateCHF;
+        }
+    };
+
+    const updateResultText = (amount, result, currency) => {
+        const resultElement = document.querySelector(".js-result");
+        resultElement.innerHTML = `${amount.toFixed(2)} PLN = <strong>${result.toFixed(2)} ${currency}</strong>`
+    };
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const amountElement = document.querySelector(".js-amount");
+        const currencyElement = document.querySelector(".js-currency");
+
+        const amount = +amountElement.value;
+        const currency = currencyElement.value;
+
+        const result = calculateResult(amount, currency);
+
+        updateResultText(amount, result, currency);
+    };
+
+    const init = () => {
+        const exchangeButton = document.querySelector(".js-exchangeButton");
+        exchangeButton.addEventListener("click", toggleCurrency);
+
+        const formElement = document.querySelector(".js-form");
+        formElement.addEventListener("submit", onFormSubmit);
+    };
+
+    welcome();
+    init();
+}
